aa71deef4dd411482309fb5a96853937f2ccc7fd
QuanAloha <nathaniel.paul.2018@icloud.com>
2021-12-08 18:51:08 -0700

    Adding Starter Code for the Levels

--- Level_1.py
+++ Level_1.py
@@ -6,3 +6,11 @@ Description of Level:
 The code in this file is written by Nathaniel Atwood and Megan Hays.
 Any distribution of this code without written consent is strictly prohibited.
 """
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
